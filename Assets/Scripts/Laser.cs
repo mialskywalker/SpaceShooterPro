5d6d4b6a63b16333bf1d7e8ceda6e9e24d5269c6
mialskywalker <kalqga123@gmail.com>
2023-09-23 13:55:22 +0300

    laser destroy

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -17,5 +17,10 @@ public class Laser : MonoBehaviour
     void Update()
     {
         transform.Translate( Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 8)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
