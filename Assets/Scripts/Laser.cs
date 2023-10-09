137500e227f68785e7f0243e669e062ebae0b8df
mialskywalker <kalqga123@gmail.com>
2023-10-09 19:51:28 +0300

    destroy triple shot

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -14,6 +14,10 @@ public class Laser : MonoBehaviour
 
         if (transform.position.y >= 8)
         {
+            if (transform.parent)
+            {
+                Destroy(transform.parent.gameObject);
+            }
             Destroy(this.gameObject);
         }
     }
